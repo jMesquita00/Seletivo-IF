aa9fc05aef6554f50235519c71831f2c6dd0e8d1
jMesquita00 <jaoa.mesquita@acad.ifma.edu.br>
2025-12-02 00:23:04 -0300

    enviando a tela de simulado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:seletivo_if/shared/widgets/buton.dart';
-// import 'package:seletivo_if/modules/auth/login_page.dart';
-// import 'package:seletivo_if/shared/widgets/card_disciplin.dart';
-import 'package:seletivo_if/shared/widgets/input.dart';
+import 'package:seletivo_if/modules/auth/createAccountPage.dart';
+import 'package:seletivo_if/modules/simulated/simulated.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,95 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'App Inicial',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomePage(),
+      home: const Simulated(),
+      // home: const CreatAccountPage(),
+      // home: const login_page(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text(''), centerTitle: true),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Image.asset("lib/shared/assets/LogoIF.png"),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-
-              child: Text(
-                "Quem se prepara chega lá",
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-
-            // CardDisciplina(
-            //   color: Colors.black,
-            //   icon:Icons.ac_unit_sharp,
-            //    name: 'matematica',)  
-            // My_Input(conteudo: 'E-mail', icone: Icons.email)
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: MyInput(
-                label: 'Name',
-                nomeController: TextEditingController(),
-              ),
-            ),
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: MyInput(
-                label: 'E-mail',
-                nomeController: TextEditingController(),
-              ),
-            ),
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: MyInput(
-                label: 'Password',
-                nomeController: TextEditingController(),
-              ),
-            ),
-            SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: MyInput(
-                label: 'Confirm Password',
-                nomeController: TextEditingController(),
-              ),
-            ),
-            SizedBox(height: 30),
-
-            MyButton(
-              conteudo: ("Criar conta"),
-              corFundo: Colors.green,
-              corTexto: Colors.white,
-              onPressed: () {
-                print("enviando");
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
--- lib/shared/widgets/buton.dart
+++ lib/shared/widgets/buton.dart
@@ -5,6 +5,7 @@ class MyButton extends StatelessWidget {
   final VoidCallback onPressed;
   final Color corFundo;
   final Color corTexto;
+  final bool fundo;
 
   const MyButton({
     super.key,
@@ -12,18 +13,19 @@ class MyButton extends StatelessWidget {
     required this.corFundo,
     required this.corTexto,
     required this.onPressed,
+    required this.fundo,
   });
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 336,
+      width: 300,
       height: 50,
       child: Container(
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.3), // sombra com opacidade
+              color: Colors.black.withValues(alpha: 0.3), // sombra com opacidade
               blurRadius: 6,
               offset: const Offset(2, 4),
             ),
@@ -33,7 +35,7 @@ class MyButton extends StatelessWidget {
         child: ElevatedButton(
           onPressed: onPressed,
           style: ElevatedButton.styleFrom(
-            backgroundColor: corFundo,
+            backgroundColor: fundo ? corFundo : null,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(100),
             ),
